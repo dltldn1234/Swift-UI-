0e794db9b95132bab8409c65e15a479cd06bd0a2
이시우 <s40409841@gmail.com>
2026-04-08 18:27:27 +0900

    Initial Commit

---
+++ Swift UI/Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UIApp.swift
+//  Swift UI
+//
+//  Created by 이시우 on 4/8/26.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

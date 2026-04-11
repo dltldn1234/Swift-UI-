4299d29b831a1f000282437730825bdbf55b055d
이시우 <s40409841@gmail.com>
2026-04-12 00:45:46 +0900

    book-on 2

---
+++ Swift UI/book-onView2.swift
@@ -0,0 +1,80 @@
+//
+//  book-onView2.swift
+//  Swift UI
+//
+//  Created by 이시우 on 4/9/26.
+//
+
+import SwiftUI
+
+struct book_onView2: View {
+    var body: some View {
+        VStack(){
+            ZStack(){
+                Color.black.ignoresSafeArea(.all)
+                
+                VStack(spacing: 0){
+                    HStack(spacing: 0){
+                        Image("Image")
+                            .padding(.trailing, 9)
+                        Text("book-on")
+                            .font(.system(size: 17, weight: .semibold))
+                            .foregroundColor(.white)
+                        
+                        Spacer()
+                        
+                    }
+                    .padding(.leading, 25)
+                    Spacer()
+                }
+                
+                ZStack(){
+                    Rectangle()
+                        .fill(Color.차콜그레이)
+                        .frame(width: 300, height: 300)
+                        .cornerRadius(25)
+                    
+                    VStack(spacing: 0){
+                        Image("Image")
+                            .padding(.bottom, 13)
+                            .padding(.top, 15)
+                        
+                            Text("Book-on에 가입하신 걸 환영합니다!")
+                                .font(.system(size: 15, weight: .bold))
+                                .foregroundColor(.white)
+                                .padding(.bottom, 54)
+                            
+                            
+                            Text("책 검색, 대출현황, 좌석예약까지 \n 한번에 관리하세요")
+                                .font(.system(size: 15, weight: .medium))
+                                .foregroundColor(.white)
+                                .padding(.bottom, 61)
+                        
+                            
+                            Text("로그인")
+                                .frame(width: 200, height: 35)
+                                .font(.system(size: 17, weight: .bold))
+                                .background(.white)
+                                .cornerRadius(10)
+                                        
+                                
+
+                                .padding(.bottom, 9)
+                        
+                            Text("처음이신가요?")
+                                .font(.system(size: 13, weight: .regular))
+                                .foregroundColor(.확인했슴다)
+                                
+                    }
+                }
+                .padding(.bottom, 165)
+                .padding(.leading, 51)
+                .padding(.trailing, 51)
+            }
+        }
+    }
+}
+
+#Preview {
+    book_onView2()
+}
